996920c0a1fe8b28510c46b2c3236355893cce00
Morolake Orekoya <morolake.orekoya@andela.com>
2018-09-21 00:11:57 +0100

    add prototype pattern

---
+++ prototype.rb
@@ -0,0 +1,36 @@
+# Intent
+# Specify the kinds of objects to create using a prototypical instance, and
+# create new objects by copying this prototype.
+
+class Prototype
+  def _clone
+    raise NotImplementedError, 'clone() must be defined in subclass'
+  end
+end
+
+class PizzaPrototype < Prototype
+  def _clone
+    return self.clone
+  end
+
+  def pizza_type(menu)
+    case menu
+    when :pepperoni
+      Pizza.new('Pepperoni Pizza', ['pepperoni', 'shredded cheese'])
+    else
+      Pizza.new('Cheese Pizza', ['cheese'])
+    end
+  end
+end
+
+class Pizza
+  def initialize(*args)
+    @name = args[0]
+    @ingredients = args[1..5]
+    puts @name, @ingredients
+  end
+end
+
+pizza_proto = PizzaPrototype.new
+pizza = pizza_proto._clone
+pizza.pizza_type(:pepperoni)
