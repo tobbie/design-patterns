8de3facb95ea9b87cbc622d7d204020266e66e0f
Morolake Orekoya <morolake.orekoya@andela.com>
2018-09-21 00:43:23 +0100

    add bridge pattern

---
+++ bridge.rb
@@ -0,0 +1,36 @@
+# Intent
+# Decouple an abstraction from its implementation so that the two can vary
+# independently.
+
+class Shape
+  def initialize(implementor)
+    @implementor = implementor
+  end
+end
+
+class Rectangle < Shape
+  def draw
+    @implementor.draw_rectangle
+  end
+end
+
+class ShapeImplementor
+  def draw_rectangle
+    raise "draw_rectangle must be implemented"
+  end
+end
+
+class Windows < ShapeImplementor
+  def draw_rectangle
+    p "drawing a rectangle on windows"
+  end
+end
+
+class MacOs < ShapeImplementor
+  def draw_rectangle
+    p "drawing a rectangle upon a Mac"
+  end
+end
+
+rectangle = Rectangle.new(MacOs.new)
+rectangle.draw
