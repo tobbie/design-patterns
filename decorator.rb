ae3f8d1a1d6f4c2cc8557b718cd03b87b849e3a0
Morolake Orekoya <morolake.orekoya@andela.com>
2018-09-24 21:49:22 +0100

    add decorator design pattern

---
+++ decorator.rb
@@ -0,0 +1,38 @@
+# Intent
+# Attach additional responsibilities to an object dynamically. Decorators
+# provide a flexible alternative to subclassing for extending functionality.
+
+class Coffee
+  attr_accessor :price
+
+  def price
+    2.50
+  end
+end
+
+class CoffeeWithCream
+  def initialize(coffee)
+    @coffee = coffee
+  end
+
+  def price
+    @coffee.price += 0.50
+  end
+end
+
+class CoffeeWithSugar
+  def initialize(coffee)
+    @coffee = coffee
+  end
+
+  def price
+    @coffee.price += 0.25
+  end
+end
+
+coffee = Coffee.new
+puts "Plain Coffee - #{coffee.price}"
+coffee = CoffeeWithCream.new(Coffee.new)
+puts "Coffee with Cream - #{coffee.price}"
+coffee = CoffeeWithSugar.new(Coffee.new)
+puts "Coffee with Sugar - #{coffee.price}"
