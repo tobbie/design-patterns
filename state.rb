381d9b2ec4296df7011e2333da585ba8979501a3
Morolake Orekoya <morolake.orekoya@andela.com>
2018-09-24 22:14:16 +0100

    add state to design patterns

---
+++ state.rb
@@ -0,0 +1,48 @@
+# Intent
+# Allow an object to alter its behaviour when its internal state changes.
+# The object will appear to change its class.
+
+class Context
+  def initialize(item)
+    @state = item
+  end
+
+  def set_state(value)
+    @state = value
+    puts "Inside Context"
+  end
+
+  def get_state
+    @state
+  end
+
+  def request
+    @state.handle(self)
+  end
+end
+
+class State
+  def handle(context)
+    raise NotImplementedError, "handle must be defined in subclass"
+  end
+end
+
+class ConcreteStateA < State
+  def handle(context)
+    puts "Handling ConcreteStateA"
+    context.set_state(ConcreteStateB.new)
+  end
+end
+
+class ConcreteStateB < State
+  def handle(context)
+    puts "Handling ConcreteStateB"
+    context.set_state(ConcreteStateA.new)
+  end
+end
+
+state = ConcreteStateA.new
+context = Context.new(state)
+
+context.request
+context.request
