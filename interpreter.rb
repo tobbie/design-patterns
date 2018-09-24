6c8810246b916fdb624a7d8c40e695da5c113edd
Morolake Orekoya <morolake.orekoya@andela.com>
2018-09-24 18:15:38 +0100

    add interpreter design pattern

---
+++ interpreter.rb
@@ -0,0 +1,47 @@
+# Intent
+# Given a language, define a representation for its grammar along with an
+# interpreter that uses the representation to interpret sentences in the language.
+
+require 'find'
+
+class AbstractExpression
+  def interpret(context)
+    raise NotImplementedError, "interpret() must be defined in subclass"
+  end
+end
+
+class TerminalExpression < AbstractExpression
+  def interpret(context)
+    puts "Inside TerminalExpression"
+  end
+end
+
+class NonTerminalExpression < AbstractExpression
+  def interpret(context)
+    puts "NonTerminalExpression"
+  end
+end
+
+class Context
+  def initialize(value)
+    @expression = value
+  end
+
+  def set_expression(value)
+    @expression = value
+  end
+
+  def get_expression
+    @expression
+  end
+end
+
+context_object = Context.new("AAAAAABBBBCCCCDDDD")
+expression_list = Array.new
+expression_list << TerminalExpression.new
+expression_list << NonTerminalExpression.new
+expression_list << TerminalExpression.new
+
+for expression in expression_list
+  expression.interpret(context_object)
+end
